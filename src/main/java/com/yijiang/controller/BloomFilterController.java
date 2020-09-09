10d288b5fb42e8710a8ba169e1df5cb399c2c8fb
821407805 <821407805@qq.com>
2020-09-09 21:39:35 +0800

    新增谷歌布隆过滤器、redis布隆过滤器

--- src/main/java/com/yijiang/controller/BloomFilterController.java
+++ src/main/java/com/yijiang/controller/BloomFilterController.java
@@ -16,17 +16,35 @@ public class BloomFilterController {
 
     @Resource
     private RedisService redisService;
-
+    /**
+     * @author jasonxiao
+     * @description  谷歌布隆过滤器
+     * @date 2020/9/9
+     * @param 
+     * @return 
+     */
     @RequestMapping("/bloom/idExists")
     public boolean ifExists(int id){
         return bloomFilterService.userIdExists(id);
     }
-
+    /**
+     * @author jasonxiao
+     * @description  redis 隆过滤器 add
+     * @date 2020/9/9
+     * @param
+     * @return
+     */
     @RequestMapping("/bloom/redisIdAdd")
     public boolean redisidAdd(String key, String value){
         return redisService.bloomFilterAdd(key, value);
     }
-
+    /**
+     * @author jasonxiao
+     * @description  redis 隆过滤器 exiests
+     * @date 2020/9/9
+     * @param
+     * @return
+     */
     @RequestMapping("/bloom/redisIdExists")
     public boolean redisidExists(String key, String value){
         return redisService.bloomFilterExists(key, value);
